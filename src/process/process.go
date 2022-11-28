e49b42bc6bd181b84ce135f346581060c1418314
Monk <kenjiyamane0@gmail.com>
2022-11-27 21:57:00 -0300

    remove init id from message log

--- src/process/process.go
+++ src/process/process.go
@@ -208,7 +208,7 @@ func (p *Process) snapshotInitiate() {
 }
 
 func (p *Process) sendMessage(outId int, messageType messages.MessageType, weight float64, initId int, initiatedAt int) {
-	fmt.Printf("sending message %v from %v to %v - initId: %v\n", messageType, p.id, outId, initId)
+	fmt.Printf("sending message %v from %v to %v\n", messageType, p.id, outId)
 	if outId == p.id {
 		switch messageType {
 		case messages.Short:
